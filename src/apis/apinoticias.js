ac74434ee3dcc5b8ea8a44cd5695da5e757a40a9
kernel <86926580+kernelboy34@users.noreply.github.com>
2024-06-03 16:26:08 -0400

    Update apinoticias.js

--- src/apis/apinoticias.js
+++ src/apis/apinoticias.js
@@ -4,11 +4,8 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 3031;
-
-// URL de conexión a MongoDB
-const url = 'mongodb+srv://lalo:bot_criptoP8@cluster0.ust518c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url = 'variable de entorno';
 const dbName = 'bot_de_tradings';
-
 // Middleware para conectar a la base de datos antes de manejar las solicitudes
 app.use(cors());
 
@@ -26,12 +23,10 @@ app.use(async (req, res, next) => {
 
 app.get('/noticias_completas', async (req, res) => {
   const { resul } = req.query;
-  
+  //lo modifican si quieren
   try {
     const db = req.db;
     const coleccion = db.collection('resultados_de_comparasioon');
-
-    // Consultar los datos en la colección
     const resultado = await coleccion.find({ cripto: resul }).toArray();
     if (resultado.length > 0) {
       res.json(resultado);
